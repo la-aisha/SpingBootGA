5b81e2bff0cec9ff17678758c2cb03d8d68f8496
la-aisha <sulamaish4738@gmail.com>
2024-03-16 21:09:52 +0100

    commit test

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome !";
+		return "Welcome Aisha!";
 	}
 
 //	echo "# github-actions-example" >> README.md
